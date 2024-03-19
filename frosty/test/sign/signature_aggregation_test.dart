f6502a69abb45967a37d6dc1bd13d9ecbb209bd5
Matthew Mitchell <mattlmitchell@proton.me>
2024-03-19 20:27:26 +0000

    Add invalid aggregation tests
    Ability to determine dishonest signer is not yet possible

--- frosty/test/sign/signature_aggregation_test.dart
+++ frosty/test/sign/signature_aggregation_test.dart
@@ -46,9 +46,69 @@ void main() {
     );
 
     test("identifiable invalid share", () {
+
+      // Use incorrect share for participant 2
+      final sharesWithInvalid = [
+        (Identifier.fromUint16(1), getShare(part1s, 0)),
+        (
+          Identifier.fromUint16(2),
+          SignatureShare.fromBytes(
+            hexToBytes(
+              "84ee1adfe96d4670fc6fcf5def51bbfa886389f5bdb2e9b3ccf91824324e613c",
+            ),
+          ),
+        ),
+      ];
+
+      SignatureAggregation(
+        commitments: getSignatureCommitments(part1s),
+        details: SignDetails(message: signMsgHash),
+        shares: sharesWithInvalid,
+        publicInfo: publicInfo,
+      );
+
     });
 
     test("invalid inputs", () {
+
+      // Incorrect number of commitments
+      expect(
+        () => SignatureAggregation(
+          commitments: SigningCommitmentSet([
+            (Identifier.fromUint16(1), part1s[0].commitment),
+          ]),
+          details: SignDetails(message: signMsgHash),
+          shares: getShares(null),
+          publicInfo: publicInfo,
+        ),
+        throwsA(isA<InvalidAggregation>()),
+      );
+
+      // Incorrect number of shares
+      expect(
+        () => SignatureAggregation(
+          commitments: getSignatureCommitments(part1s),
+          details: SignDetails(message: signMsgHash),
+          shares: getShares(null).take(1).toList(),
+          publicInfo: publicInfo,
+        ),
+        throwsA(isA<InvalidAggregation>()),
+      );
+
+      // Shares from wrong identifiers
+      expect(
+        () => SignatureAggregation(
+          commitments: SigningCommitmentSet([
+            (Identifier.fromUint16(2), part1s[1].commitment),
+            (Identifier.fromUint16(3), part1s[2].commitment),
+          ]),
+          details: SignDetails(message: signMsgHash),
+          shares: getShares(null),
+          publicInfo: publicInfo,
+        ),
+        throwsA(isA<InvalidAggregation>()),
+      );
+
     });
 
   });
