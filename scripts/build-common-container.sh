d013320a07f4802ca0b52212ad4b6bdf9abbea40
Matthew Mitchell <mattlmitchell@proton.me>
2026-06-11 15:27:50 +0100

    Create shared build function for linux and wasm

--- scripts/build-common-container.sh
+++ scripts/build-common-container.sh
@@ -2,6 +2,9 @@
 
 . $THISDIR/build-common.sh
 
+# Ensure build() is exported
+set -a
+
 # Prefer podman
 if type podman > /dev/null; then
     PROGCMD=podman
@@ -14,3 +17,35 @@ fi
 # Build common build image
 $PROGCMD build -f $THISDIR/build_common.Dockerfile -t frosty_build_common $THISDIR/.. \
     || exit 1
+
+# The function that builds for linux or wasm
+build () {
+    echo "Building for $1"
+    mkdir -p $2
+    # Run container leaving build result in proper place
+    $PROGCMD run --rm \
+        --volume $OUTPUTDIR/$2:/output:Z \
+        $TAG bash -c \
+        "cargo build -r --target $1 && \
+        cp /build/target/$1/release/$LIBNAME /output/$LIBNAME && \
+        if [ \"$2\" = \"wasm\" ]; then \
+            mkdir -p /output/pkg && \
+            wasm-bindgen --target no-modules \
+              --out-name $WASM_BINDGEN_OUTNAME \
+              --out-dir /output/pkg \
+              /build/target/$1/release/$LIBNAME; \
+        fi" \
+        || exit 1
+    if [ "$2" = "wasm" ]; then
+      echo "Copying wasm-bindgen outputs to $WEB_OUTDIR"
+      mkdir -p "$WEB_OUTDIR"
+      cp "$2/pkg/$WASM_BINDGEN_OUTNAME.js" "$WEB_OUTDIR"
+      cp "$2/pkg/${WASM_BINDGEN_OUTNAME}_bg.wasm" "$WEB_OUTDIR"
+    else
+      echo "Copying to frosty/build for local use"
+      cp $2/$LIBNAME $LOCALBUILDDIR/$LIBNAME
+    fi
+    echo "Archiving $1"
+    tar -czvf frosty-$VERSION-$2.tar.gz -C $2 .
+    rm -r $2
+}
--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -5,46 +5,11 @@ THISDIR=$(dirname "$(realpath "$0")")
 
 LIBNAME=frosty_rust.wasm
 WASM_BINDGEN_OUTNAME=frosty_rust
-WEB_OUTDIRS=(
-    "$THISDIR/../frosty_flutter/example/web/pkg"
-    "$THISDIR/../frosty_flutter/web/pkg"
-)
+WEB_OUTDIR=$THISDIR/../frosty_flutter/web/pkg
 
 # Build container image with the build context being the parent directory
 TAG=frosty_rust_wasm_build
 $PROGCMD build -f $THISDIR/build_wasm.Dockerfile -t $TAG $THISDIR/.. || exit 1
-
-build () {
-    echo "Building for $1"
-    mkdir -p $2
-    # Run container leaving build result in proper place
-    $PROGCMD run --rm \
-        --volume $OUTPUTDIR/$2:/output:Z \
-        $TAG bash -c \
-        "cargo build -r --target $1 && \
-        cp /build/target/$1/release/$LIBNAME /output/$LIBNAME && \
-        if [ \"$2\" = \"wasm\" ]; then \
-            mkdir -p /output/pkg && \
-            wasm-bindgen --target no-modules \
-              --out-name $WASM_BINDGEN_OUTNAME \
-              --out-dir /output/pkg \
-              /build/target/$1/release/$LIBNAME; \
-        fi" \
-        || exit 1
-    echo "Copying to frosty/build for local use"
-    cp $2/$LIBNAME $LOCALBUILDDIR/$LIBNAME
-    if [ "$2" = "wasm" ]; then
-        for web_outdir in "${WEB_OUTDIRS[@]}"; do
-            echo "Copying wasm-bindgen outputs to $web_outdir"
-            mkdir -p "$web_outdir"
-            cp "$2/pkg/$WASM_BINDGEN_OUTNAME.js" "$web_outdir/"
-            cp "$2/pkg/${WASM_BINDGEN_OUTNAME}_bg.wasm" "$web_outdir/"
-        done
-    fi
-    echo "Archiving $1"
-    tar -czvf frosty-$VERSION-$2.tar.gz -C $2 . 
-    rm -r $2
-}
 
 # Build for wasm32-unknown-unknown
 build wasm32-unknown-unknown wasm
